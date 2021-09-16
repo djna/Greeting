4275252a714aca37f4b9ef701d58dc52b727b5cc
Shivam Chandarana <shivam.chandarana@softwire.com>
2021-09-16 11:02:49 +0100

    Parametrise name and game and fix typo

--- education/src/org/djna/Greeting.java
+++ education/src/org/djna/Greeting.java
@@ -3,7 +3,9 @@ package org.djna;
 public class Greeting {
     public static void main(String[] args) {
 
-        System.out.println("Hellow I'm Dave and I play Go");
+        String name = "Dave";
+        String game = "Go";
+        System.out.println("Hello I'm " + name + " and I play " + game);
 
     }
 }
